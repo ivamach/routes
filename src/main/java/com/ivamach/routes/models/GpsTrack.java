de624a7b1ab9606ec6b2b19be8aed8e9a9e13a21
ivamach <ivana.machacikova@gmail.com>
2024-05-21 18:57:32 +0200

    GpsTrack added

---
+++ src/main/java/com/ivamach/routes/models/GpsTrack.java
@@ -0,0 +1,19 @@
+package com.ivamach.routes.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GpsTrack {
+
+  private List<GpsPoint> points = new ArrayList<>();
+
+}
