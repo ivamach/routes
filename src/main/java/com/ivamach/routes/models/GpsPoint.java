4224714e373e6d0404fdcdf6f8fd303166b80a7b
ivamach <ivana.machacikova@gmail.com>
2024-05-21 18:56:27 +0200

    GpsPoint added

---
+++ src/main/java/com/ivamach/routes/models/GpsPoint.java
@@ -0,0 +1,16 @@
+package com.ivamach.routes.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GpsPoint {
+  private double x;
+  private double y;
+  private double t;
+}
